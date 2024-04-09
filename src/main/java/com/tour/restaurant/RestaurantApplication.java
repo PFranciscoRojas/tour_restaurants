b64cb81ac1ee4dc5daabb51665f1484566d4765a
Camilo Tello <82854269+Ckm1204@users.noreply.github.com>
2024-04-09 18:00:35 -0500

    Create  RestaurantApplication.java

---
+++ src/main/java/com/tour/restaurant/RestaurantApplication.java
@@ -0,0 +1,16 @@
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+    @SpringBootApplication
+    public static class RestaurantApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(RestaurantApplication.class, args);
+        }
+
+    }
+}
