2e75114761c7e26dc773101b2f577b3d283bcfb0
AT1927 <atmunoz1927@gmail.com>
2024-04-10 16:47:10 -0500

    update repositorys for entities

--- src/main/java/com/tour/restaurant/Domain/Repository/RestaurantRepository.java
+++ src/main/java/com/tour/restaurant/Domain/Repository/RestaurantRepository.java
@@ -3,7 +3,15 @@ import com.tour.restaurant.infraestructure.Entities.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
     // Aquí puedes agregar métodos adicionales si necesitas consultas personalizadas
+    List<Restaurant> getAll();
+    List<Restaurant> getByRestaurant(Restaurant restaurant);
+    Optional<Restaurant> getById(long id);
+    void deleteByID(long id);
+    //Restaurant save(Restaurant restaurant);
 }
