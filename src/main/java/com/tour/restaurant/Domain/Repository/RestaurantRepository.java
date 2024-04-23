90da41f8b4944e951fbc4856b4ad4a0bd761404d
AT1927 <atmunoz1927@gmail.com>
2024-04-23 11:22:56 -0500

    Modified class RestaurantRepository

--- src/main/java/com/tour/restaurant/Domain/Repository/RestaurantRepository.java
+++ src/main/java/com/tour/restaurant/Domain/Repository/RestaurantRepository.java
@@ -1,12 +1,12 @@
 package com.tour.restaurant.Domain.Repository;
 import com.tour.restaurant.infraestructure.Entities.Restaurant;
-import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+public interface RestaurantRepository {
     // Aquí puedes agregar métodos adicionales si necesitas consultas personalizadas
     List<Restaurant> getAll();
     List<Restaurant> getByRestaurant(Restaurant restaurant);
